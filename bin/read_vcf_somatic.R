107bf572814154ac537d9fe65f44a18c1bbb5d86
Lucrezia Valeriani <valerianilucrezia11@gmail.com>
2024-09-11 12:34:23 +0200

    Update read_vcf_somatic.R

--- bin/read_vcf_somatic.R
+++ bin/read_vcf_somatic.R
@@ -4,13 +4,12 @@ library(vcfR)
 
 args <- commandArgs(trailingOnly = TRUE)
 file <- args[1]         # vcf file
-#res_dir <- args[2]      # samplename
 
 vcf <- vcfR::read.vcfR(file)
 vcf <- vcfR::vcfR2tidy(vcf)
 fix <- vcf$fix %>% dplyr::mutate(id = paste(ChromKey, POS, sep = ':')) %>% select(id, CHROM, POS, REF, ALT, FILTER)
 gt <- vcf$gt %>% dplyr::mutate(id = paste(ChromKey, POS, sep = ':')) %>% select(id, gt_DP, gt_AF, gt_AD)
-vaf <- full_join(fix, gt)
+vaf <- full_join(fix, gt) %>% select(-ChromKey)
 
 base_name <- sub('\\.vcf.gz*$', '', basename(file))
 output_file <- paste0(base_name, '_vaf.RDS')
