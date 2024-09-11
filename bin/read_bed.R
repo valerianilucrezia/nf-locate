6ed1d42db6ddedce7dd44222fe42c41ad4aacb88
Lucrezia Valeriani <valerianilucrezia11@gmail.com>
2024-09-11 12:47:50 +0200

    Update read_bed.R

--- bin/read_bed.R
+++ bin/read_bed.R
@@ -3,29 +3,15 @@ library(dplyr)
 library(data.table)
 library(tidyr)
 
-# Function to process data for a single chromosome
-read_meth <- function(data, ch, out) {
-  f_file <- data %>%
-    filter(chr == ch) %>%
-    separate(N, into = c("cov", "beta", 'mod', 'canon', 'other', 'delete', 'fail', 'diff', 'nocall'), sep = " ")
-  
-  saveRDS(f_file, paste0(out, '.RDS'))
-}
-
-
-######################################################
-### Processing a single chromosome
-######################################################
 args <- commandArgs(trailingOnly = TRUE)
-out <- args[3] # output dir / name
-bed_file <- args[2]   # path to the single bed file
-chr <- args[1] # chromosome
+bed <- args[1]   # path to the bed file
 
-file <- fread(bed_file, nThread = 2)
-colnames(file) <- c('chr', 'start', 'end', 'mod', 'score', 'strand', 'v1', 'v2', 'v3', 'N','aux1', 'aux2', 'aux3', 'aux4', 'aux5', 'aux6', 'aux7', 'aux8')
-
+file <- fread(bed, nThread = getDTthreads())
+colnames(file) <- c('chr', 'start', 'end', 'mod', 'score', 'strand', 'v1', 'v2', 'v3', "cov", "beta", 'mod', 'canon', 'other', 'delete', 'fail', 'diff', 'nocall')
 data <- file %>%
-  filter(chr == chr) %>%
-  select(-v1, -v2, -v3, -mod)
+  dplyr::select(-v1, -v2, -v3, -mod, -strand, -start) %>%
+  dplyr::rename(POS = end, CHROM = chr)
+  #separate(N, into = c("cov", "beta", 'mod', 'canon', 'other', 'delete', 'fail', 'diff', 'nocall'), sep = " ")
 
-read_meth(data, chr, out)
+basename <- sub('\\.bed*$', '', basename(bed))
+saveRDS(object = data, file = paste0(basename, '.RDS'))
