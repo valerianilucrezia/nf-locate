f1f01261fb4671f8b9d43f0a2fea64026660a8df
Lucrezia Valeriani <valerianilucrezia11@gmail.com>
2024-09-11 12:34:44 +0200

    Update read_vcf_somatic.R

--- bin/read_vcf_somatic.R
+++ bin/read_vcf_somatic.R
@@ -9,7 +9,7 @@ vcf <- vcfR::read.vcfR(file)
 vcf <- vcfR::vcfR2tidy(vcf)
 fix <- vcf$fix %>% dplyr::mutate(id = paste(ChromKey, POS, sep = ':')) %>% select(id, CHROM, POS, REF, ALT, FILTER)
 gt <- vcf$gt %>% dplyr::mutate(id = paste(ChromKey, POS, sep = ':')) %>% select(id, gt_DP, gt_AF, gt_AD)
-vaf <- full_join(fix, gt) %>% select(-ChromKey)
+vaf <- full_join(fix, gt) %>% select(-id)
 
 base_name <- sub('\\.vcf.gz*$', '', basename(file))
 output_file <- paste0(base_name, '_vaf.RDS')
